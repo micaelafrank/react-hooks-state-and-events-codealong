9cab9df6cb9b86dfdd8fcc7acaf068a33786cf97
Micaela Frank <micaelalfrank@gmail.com>
2022-07-16 14:36:34 -0400

    commit tree 4b2df36dddb0bbdf80cdd4f553a74bc686c79edf

--- src/components/Toggle.js
+++ src/components/Toggle.js
@@ -1,7 +1,54 @@
-import React from "react";
+import React, {useState} from "react";
 
 function Toggle() {
-  return <button>OFF</button>;
+  const [isOn, setIsOn] = useState(false);
+
+  function handleToggle() {
+    setIsOn((isOn) => !isOn);
+  }
+
+  return <button style={{ background: isOn ? "red" : "white" }} onClick={handleToggle}>{isOn ? "ON" : "OFF"}</button>;
 }
 
 export default Toggle;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//   function handleButton(){
+//     switchButton((isOn) => !isOn);
+//   }
+
+//   const color = isOn ? "green" : "red";
+  
+//   return <button onClick={handleButton} style={{ background: color }}>{isOn ? "ON" : "OFF"}</button>;
+
+// }
